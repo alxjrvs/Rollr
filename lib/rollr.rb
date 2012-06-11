af89d10f693f56c234a6cf216b87d4a3b78d86af
Alex Jarvis <alxjrvs@gmail.com>
2012-06-11 13:35:47 -0400

    DiceRoll implemented

--- lib/rollr.rb
+++ lib/rollr.rb
@@ -1,5 +1,11 @@
 # Rollr Simulates rolling dice of different size.
 #
+# In its most recent version, it also supports a kind of primitive logging,
+# which can track the rolls made to the die over time, as well as the individual
+# results of rolls within a pool of rolls (so, for instance, if you rolled 20
+# 6 six sided die, you could see the result of each individual roll of each
+# die.
+#
 # @author Alex Jarvis
 
 require 'rubygems'
@@ -7,38 +13,66 @@ require 'rubygems'
 module Rollr
 
 
-  # Represents a single instance of the result of a die
+  # Represents a single instance of a die
   #
   class Die
 
 
     # @attribute sides [Integer] The number of sides the die has
     #
-    attr_accessor :sides
+    attr_accessor :sides, :rolls
 
-    # New instance of Dice.
+    # New instance of Die
     #
     # @param [Integer] sides
     # @return [Object] Die object
     #
     def initialize(sides)
       @sides = sides
+      @rolls = []
     end #initialize
 
-    # Roll a new die.
+    # Roll a new die. This creates a DiceRoll Object, and adds it to the array of rolls from this Die. 
+    # Then, return the total of that Roll.
     #
     # @param [Integer] count
-    # @return [Object] integer
+    # @return [Integer] The result of the rolls, by way of a DiceRoll object.
     #
     def roll(count=1)
       individual_rolls = (1..count).map { |d| rand(self.sides) + 1 }
-      total_roll = individual_rolls.inject(0) { |total, d| total += d }
-      return individual_rolls, total_roll
+      @rolls << DiceRoll.new(individual_rolls, self.sides, count)
+      @rolls.last.total
     end #roll
 
   end #Die
 
+  #DiceRoll is a class that holds information regarding a single instance of a rolled die.
+  #
+  class DiceRoll
 
+    # @attribute each_side [Array] the individual result of a single roll of a die.
+    # @attribute total [Integer] the combined total of the individual rolls
+    # @attribute dice_sides [Integer] the number of sides the rolled die had
+    # @attribute number_of_dice [Integer] the number of dice rolled.
+    # 
+    attr_accessor :each_die, :total, :dice_sides, :number_of_dice
+
+
+    # New instance of DiceRoll
+    #
+    # @param [Array] individual
+    # @param [Integer] sides
+    # @param [Integer] number
+    # @return [Object] Die object
+    #
+    def initialize(individual, sides, number)
+      @each_die = individual
+      @dice_sides = sides
+      @number_of_dice = number
+      @total = individual.inject(0) { |total, d| total += d }
+    end #initialize
+
+  end #DiceRoll
   #Die Constants
 
   D3 = Die.new(3)
