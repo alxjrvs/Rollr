ba14545658a7b47f9c2ca3db5190d92d8cb835cc
Alex Jarvis <alxjrvs@gmail.com>
2016-11-04 00:31:00 -0400

    Trying this?

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,6 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'rollr'
 require 'support/always_what_you_give_it'
+
 require "codeclimate-test-reporter"
 CodeClimate::TestReporter.start
