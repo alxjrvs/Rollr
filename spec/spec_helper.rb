08c8166c7d27204423e64519050414ff4883aadc
Alex Jarvis <alxjrvs@gmail.com>
2016-11-04 00:45:06 -0400

    rearranging spec helper order

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,7 @@
+require "codeclimate-test-reporter"
+CodeClimate::TestReporter.start
+
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'rollr'
 require 'support/always_what_you_give_it'
 
-require "codeclimate-test-reporter"
-CodeClimate::TestReporter.start
