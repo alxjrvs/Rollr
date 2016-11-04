27467f9018f35872935d88d16b6ca3db84d81d2b
Alex Jarvis <alxjrvs@gmail.com>
2016-11-04 00:27:31 -0400

    shhh.

---
+++ lib/rollr/metadata.rb
@@ -0,0 +1,6 @@
+module Randsum
+  SUMMARY = "RNaaOOP"
+  DESCRIPTION = "Don't you hate it when you have to generate specific quantities of random numbers from a particular range while writing ruby?  Look no further. "
+  HOMEPAGE = "https://api.randsum.io"
+  LICENSE = "MIT"
+end
